01de0c37e87826fab28b8ef4f7beed1700f39b62
xzl <23302010043@m.fudan.edu.cn>
2025-01-23 22:30:56 +0800

    feat(manager): 添加系统角色管理功能
    - 新增 SysRoleController、SysRoleMapper、SysRoleService等类
    - 实现角色列表分页查询功能
    - 创建角色映射文件 SysRoleMapper.xml
    - 编写角色服务实现类 SysRoleServiceImpl

---
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/SysRoleController.java
@@ -0,0 +1,24 @@
+package com.atguigu.spzx.manager.controller;
+
+import com.atguigu.spzx.manager.service.SysRoleService;
+import com.atguigu.spzx.model.dto.system.SysRoleDto;
+import com.atguigu.spzx.model.entity.system.SysRole;
+import com.atguigu.spzx.model.vo.common.Result;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/admin/system/sysRole")
+public class SysRoleController {
+    @Autowired
+    private SysRoleService sysRoleService;
+    @PostMapping("/findByPage/{pageNum}/{pageSize}")
+    public Result<PageInfo<SysRole>> findByPage(@RequestBody SysRoleDto sysRoleDto ,
+                                                @PathVariable(value = "pageNum") Integer pageNum ,
+                                                @PathVariable(value = "pageSize") Integer pageSize) {
+        PageInfo<SysRole> pageInfo = sysRoleService.findByPage(sysRoleDto , pageNum , pageSize) ;
+        return Result.build(pageInfo , ResultCodeEnum.SUCCESS) ;
+    }
+}
